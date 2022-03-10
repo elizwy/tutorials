eccb6c9f2dc52d23131846fd1760376b6d4baf78
eli.zwy <wenyang.zhang@ipinyou.com>
2022-03-10 21:19:14 +0800

    feat: threadLocalTest

--- javaTech/src/main/java/com/elizwy/javatech/base/ThreadLocalTest.java
+++ javaTech/src/main/java/com/elizwy/javatech/base/ThreadLocalTest.java
@@ -1,30 +1,26 @@
 package com.elizwy.javatech.base;
 
-import lombok.Synchronized;
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
 public class ThreadLocalTest {
-    static Object o = new Object();
     public static void main(String[] args) throws InterruptedException {
         final ThreadLocal<String> threadLocal = new ThreadLocal<>();
-        for (int i = 0; i < 30; i++) {
-            synchronized (o){
-                new Thread(()->{
-                    threadLocal.set(UUID.randomUUID().toString());
-                    final String result = threadLocal.get();
-                    System.out.println(result);
-                    try {
-                        o.wait();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }).start();
+        final ThreadLocal<String> threadLocal2 = new ThreadLocal<>();
+        final ThreadLocal<String> threadLocal3 = new ThreadLocal<>();
+
+        new Thread(() -> {
+            threadLocal.set(UUID.randomUUID().toString());
+            threadLocal2.set(UUID.randomUUID().toString());
+            threadLocal3.set(UUID.randomUUID().toString());
+            final String result = threadLocal.get();
+            System.out.println(result);
+            final String result2 = threadLocal2.get();
+            System.out.println(result2);
+            final String result3 = threadLocal3.get();
+            System.out.println(result3);
+            threadLocal2.remove();
+        }).start();
 
 
-            }
-        }
     }
 }
